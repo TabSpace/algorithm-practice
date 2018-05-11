0f743b72beb25dbf3254751afe55ab4207db398d
Tony.Liang <pillar0514@163.com>
2018-05-11 17:10:12 +0800

    完善qs测试结果输出格式

--- quick-sort/index.js
+++ quick-sort/index.js
@@ -21,27 +21,30 @@ const qsRuanyifeng = arr => {
 
 {
 	console.log('');
-	console.log('sample test');
+	console.log('## sample test');
 	const sample = [3, 1, 4, 1, 5, 9, 2, 6];
 	{
 		let arr = sample.slice(0);
 		let rs = arr.sort((n1, n2) => (n1 - n2));
-		console.log(rs, 'proto');
+		console.log('-', rs, 'proto');
 	}
 	{
 		let arr = sample.slice(0);
 		let rs = qsRuanyifeng(arr);
-		console.log(rs, 'ruan yi feng');
+		console.log('-', rs, 'ruan yi feng');
 	}
 }
 
 // 正式算法测试
+console.log('');
+console.log('# quick sort test');
+
 var getOriginData;
 
 {
 	const count = 1000000;
 	console.log('');
-	console.time('generate origin data');
+	console.time('- generate origin data');
 	let arr = [];
 	for (let i = 0; i < count; i++) {
 		arr.push(Math.floor(count * Math.random()));
@@ -49,9 +52,9 @@ var getOriginData;
 	let sorted = arr.slice(0);
 	sorted = arr.sort((n1, n2) => n1 - n2);
 	let max = sorted[sorted.length - 1];
-	console.log('Origin arr length:', count);
-	console.log('Origin arr max:', max);
-	console.timeEnd('generate origin data');
+	console.log('- Origin arr length:', count);
+	console.log('- Origin arr max:', max);
+	console.timeEnd('- generate origin data');
 
 	getOriginData = () => {
 		let rs = {};
@@ -62,6 +65,7 @@ var getOriginData;
 }
 
 const runTest = (fn, name) => {
+	name = '- ' + name;
 	console.time(name);
 	let rs = getOriginData();
 	let arr = rs.origin;
@@ -73,7 +77,6 @@ const runTest = (fn, name) => {
 
 {
 	console.log('');
-	console.log('quick sort test start');
 
 	// 原生排序方法
 	runTest(arr => {
---
+++ quick-sort/result.md
@@ -0,0 +1,8 @@
+# quick sort test
+
+- Origin arr length: 1000000
+- Origin arr max: 999999
+- generate origin data: 615.349ms
+
+- proto: 304.094ms
+- ruan yi feng: 509.486ms
